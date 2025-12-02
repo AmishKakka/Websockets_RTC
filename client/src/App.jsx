b1f331ec5d150fa08c53fb520d1772e3b85e5c97
Suryansh Bhupesh Purohit <suryansh.purohit3112@gmail.com>
2025-12-01 19:09:08 -0700

    added structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import { Module1, Module2, Home } from './pages';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <p>Start</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path='/module1' element={<Module1 />} />
+        <Route path='/module2' element={<Module2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
